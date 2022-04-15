86920635e4608659810df2168d7940832fd766a3
uzbedeveloper <103754533+uzbedeveloper@users.noreply.github.com>
2022-04-16 02:21:40 +0500

    Update date_time_userbot_teletips.py

--- date_time_userbot_teletips.py
+++ date_time_userbot_teletips.py
@@ -26,35 +26,18 @@ Time_Zone = os.environ["TIME_ZONE"]
 async def main_teletips():
     try:
         while True:
-            if Date_Time_Userbot_teletips.is_connected:
-                Quotes_teletips = random.choice(quotes_teletips)
-                Emojis_teletips = random.choice(emojis_teletips)
+            if Date_Time_Userbot_teletips.is_connected: 
                 TimeZone_teletips = datetime.datetime.now(pytz.timezone(f"{Time_Zone}"))
-                Time_teletips = TimeZone_teletips.strftime("   %I:%M")
+                Time_teletips = TimeZone_teletips.strftime("   %I:%M:%S")
                 Date_teletips = TimeZone_teletips.strftime("%d.%m.%Y") 
-                Image_teletips = Image.open("image.jpg")
-                Image_font_teletips = ImageFont.truetype("ds-digit.ttf", 360)
-                Image_font_teletips2 = ImageFont.truetype("ds-digit.ttf", 200)
-                Image_text_teletips = f"{Time_teletips}"
-                Image_text_teletips2 = f"{Date_teletips}"
-                Image_edit_teletips = ImageDraw.Draw(Image_teletips)
-                Image_edit_teletips.text((690, 550), Image_text_teletips, (3,160,98), font = Image_font_teletips)
-                Image_edit_teletips.text((900, 850), Image_text_teletips2, (3,160,98), font = Image_font_teletips2)
-                Image_teletips.save("Image_final_teletips.jpg")
-                await Date_Time_Userbot_teletips.update_profile(bio = f"📅Today is: {Date_teletips}" , last_name = f" ")
-                await Date_Time_Userbot_teletips.set_profile_photo(photo="Image_final_teletips.jpg")
-                me = await Date_Time_Userbot_teletips.get_me()
-                photos = await Date_Time_Userbot_teletips.get_profile_photos("me")
-                try:
-                    await Date_Time_Userbot_teletips.delete_profile_photos(photos[1].file_id)
-                except Exception:
-                    pass        
-                print("Profile Updated! Murodhonov")
-            await asyncio.sleep(60)     
+                await Date_Time_Userbot_teletips.update_profile(bio = f"📅Today is: {Date_teletips} | ⌚Time is: {Time_teletips}" , last_name = f" ") 
+                me = await Date_Time_Userbot_teletips.get_me()      
+                print("Profile Bio Updated! Murodhonov Per second")
+            await asyncio.sleep(1)     
     except FloodWait as e:
         await asyncio.sleep(e.x)         
 
-print("DATE TIME USERBOT IS ALIVE!")
+print("DATE TIME USERBOT IS ALIVE! On PerSecond")
 asyncio.ensure_future(main_teletips())
 Date_Time_Userbot_teletips.run()
 
